3994d67f7aed13d383fccdc929eba24339bc0be6
kim0hoon <gns9460@gmail.com>
2024-04-27 00:45:40 +0900

    build.gradle retrofit2 추가 https://github.com/LayTheGroundWork/Eighteen_Android/issues/2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,4 +67,8 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    //retrofit2
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.11.0")
 }

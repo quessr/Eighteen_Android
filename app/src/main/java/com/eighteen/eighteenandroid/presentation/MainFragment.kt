3e4878e2960bce5bebcd54f0051defd4da8c01ed
jeongminji4490 <hamins97@naver.com>
2024-05-02 00:44:28 +0900

    feat: 상단 하단바 태그 추가

---
+++ app/src/main/java/com/eighteen/eighteenandroid/presentation/MainFragment.kt
@@ -0,0 +1,21 @@
+package com.eighteen.eighteenandroid.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.eighteen.eighteenandroid.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMainBinding.inflate(inflater)
+        return binding.root
+    }
+}

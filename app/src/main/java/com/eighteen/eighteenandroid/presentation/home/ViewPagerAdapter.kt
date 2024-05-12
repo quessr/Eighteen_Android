465c45a3a3db48397dce603ae650460613bddc12
jeongminji4490 <hamins97@naver.com>
2024-05-13 00:07:21 +0900

    feat: 메인화면 2차 수정 #15

--- app/src/main/java/com/eighteen/eighteenandroid/presentation/home/ViewPagerAdapter.kt
+++ app/src/main/java/com/eighteen/eighteenandroid/presentation/home/ViewPagerAdapter.kt
@@ -1,10 +1,12 @@
 package com.eighteen.eighteenandroid.presentation.home
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.recyclerview.widget.RecyclerView
+import android.widget.ImageView
+import androidx.viewpager.widget.PagerAdapter
+import com.bumptech.glide.Glide
 import com.eighteen.eighteenandroid.R
 
 /**
@@ -12,25 +14,35 @@ import com.eighteen.eighteenandroid.R
  * @file ViewPagerAdapter.kt
  * @date 05/08/2024
  */
+
 class ViewPagerAdapter (
-    private var tagList: List<String>,
-) : RecyclerView.Adapter<ViewPagerAdapter.ViewHolder>() {
+    private val context: Context,
+    private val tagList: List<String>,
+) : PagerAdapter() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.today_teen_item, parent, false)
-        return ViewHolder(view)
+    private var layoutInflater: LayoutInflater? = null
+
+    override fun instantiateItem(container: ViewGroup, pos: Int): Any {
+        layoutInflater = LayoutInflater.from(context)
+        val view = layoutInflater!!.inflate(R.layout.today_teen_item, container, false)
+        val profileImg: ImageView = view.findViewById(R.id.img_today_teen)
+        Glide.with(context).load(tagList[pos]).into(profileImg);
+
+        container.addView(view, pos)
+        return view
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = tagList[position]
-        holder.title.text = item
-    }
-
-    override fun getItemCount(): Int {
+    override fun getCount(): Int {
         return tagList.size
     }
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val title: TextView = view.findViewById(R.id.tx_name)
+    override fun isViewFromObject(view: View, obj: Any): Boolean {
+        return view === obj
     }
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        val view = `object` as View
+        container.removeView(view)
+    }
+
 }

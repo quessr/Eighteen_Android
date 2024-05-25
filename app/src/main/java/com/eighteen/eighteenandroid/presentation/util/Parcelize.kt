afba7023c84a771af319f21d5461b43f9bdf68a1
kim0hoon <gns9460@gmail.com>
2024-05-25 22:01:34 +0900

    parcelize 버전분기 get 함수 추가 https://github.com/LayTheGroundWork/Eighteen_Android/issues/19

---
+++ app/src/main/java/com/eighteen/eighteenandroid/presentation/util/Parcelize.kt
@@ -0,0 +1,22 @@
+package com.eighteen.eighteenandroid.presentation.util
+
+import android.os.Build
+import android.os.Bundle
+import android.os.Parcelable
+
+@Suppress("DEPRECATION")
+fun <T> Bundle.getParcelableOrNull(key: String, clazz: Class<T>): T? =
+    if (Build.VERSION_CODES.TIRAMISU <= Build.VERSION.SDK_INT) getParcelable(key, clazz)
+    else getParcelable(key)
+
+
+@Suppress("DEPRECATION")
+fun <T : Parcelable> Bundle.getParcelableArrayListOrNull(
+    key: String,
+    clazz: Class<T>
+): ArrayList<out T>? =
+    if (Build.VERSION_CODES.TIRAMISU <= Build.VERSION.SDK_INT) getParcelableArrayList(key, clazz)
+    else getParcelableArrayList(key)
+
+fun <T : Parcelable> Bundle.getParcelableListOrNull(key: String, clazz: Class<T>): List<T>? =
+    getParcelableArrayListOrNull(key, clazz)?.toList()

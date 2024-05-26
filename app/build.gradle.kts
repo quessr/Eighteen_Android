f4fdfd81721d65e494b293eec229d6028b8f8799
kim0hoon <gns9460@gmail.com>
2024-05-26 21:47:25 +0900

    media3 추가 https://github.com/LayTheGroundWork/Eighteen_Android/issues/19

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -105,4 +105,10 @@ dependencies {
 
     //kotlin ext
     implementation("androidx.fragment:fragment-ktx:1.7.0")
+
+    //media3
+    val media3Version = "1.3.1"
+    implementation("androidx.media3:media3-exoplayer:$media3Version")
+    implementation("androidx.media3:media3-ui:$media3Version")
+    implementation("androidx.media3:media3-common:$media3Version")
 }

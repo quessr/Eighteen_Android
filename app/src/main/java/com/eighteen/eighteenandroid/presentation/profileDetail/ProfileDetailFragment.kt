2a26cd37df14922b2a7fc5c6964f018e87ee41ea
quessr <heosera76@gmail.com>
2024-06-02 00:50:08 +0900

    커스텀 dotIndicator가 포함된 이미지 슬라이드 구현
    #20
    - 커스텀 ViewPagerAdapter를 통해 프로필 이미지를 위한 슬라이드 기능과 탭 간의 연동을 구현
    - TabLayout에 커스텀 dotIndicator를 추가

--- app/src/main/java/com/eighteen/eighteenandroid/presentation/profileDetail/ProfileDetailFragment.kt
+++ app/src/main/java/com/eighteen/eighteenandroid/presentation/profileDetail/ProfileDetailFragment.kt
@@ -1,32 +1,56 @@
 package com.eighteen.eighteenandroid.presentation.profileDetail
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
 import com.eighteen.eighteenandroid.R
 import com.eighteen.eighteenandroid.databinding.FragmentProfileDetailBinding
 import com.eighteen.eighteenandroid.presentation.BaseFragment
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
-class ProfileDetailFragment : BaseFragment<FragmentProfileDetailBinding>(FragmentProfileDetailBinding::inflate) {
+class ProfileDetailFragment :
+    BaseFragment<FragmentProfileDetailBinding>(FragmentProfileDetailBinding::inflate) {
 
+    private lateinit var viewPager: ViewPager2
+    private lateinit var tabLayout: TabLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+
     override fun initView() {
         initNavigation()
+        setupViewPagerAndTabs()
+    }
+
+    private fun setupViewPagerAndTabs() {
+        val items = listOf(
+            "https://cdn.seoulwire.com/news/photo/202109/450631_649892_1740.jpg",
+            "https://contents-cdn.viewus.co.kr/image/2023/12/CP-2022-0017/image-de4d5a79-bbe3-4c2e-84a7-f36976345663.jpeg",
+            "https://cdn.hankooki.com/news/photo/202309/107376_146623_1695826504.jpg",
+            "https://cdn.dailycc.net/news/photo/202312/766253_670987_1515.png",
+            "https://cdn.newsculture.press/news/photo/202306/525899_650590_620.jpg",
+        )
+
+        bind { viewPager.adapter = ViewPagerAdapter(items) }
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position -> }.attach()
     }
 
     private fun initNavigation() {
         bind {
-            btnGoMain.setOnClickListener {
-                val navController =
-                    Navigation.findNavController(requireActivity().findViewById(R.id.fragment_container_view))
-                navController.navigate(R.id.action_fragmentProfileDetail_to_fragmentMain)
-            }
+//            btnGoMain.setOnClickListener {
+//                val navController =
+//                    Navigation.findNavController(requireActivity().findViewById(R.id.fragment_container_view))
+//                navController.navigate(R.id.action_fragmentProfileDetail_to_fragmentMain)
         }
     }
 }
---
+++ app/src/main/java/com/eighteen/eighteenandroid/presentation/profileDetail/ViewPagerAdapter.kt
@@ -0,0 +1,33 @@
+package com.eighteen.eighteenandroid.presentation.profileDetail
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.eighteen.eighteenandroid.R
+
+class ViewPagerAdapter(private val items: List<String>) : RecyclerView.Adapter<ViewPagerAdapter.ViewPagerViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewPagerViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.page_item, parent, false)
+        return ViewPagerViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onBindViewHolder(holder: ViewPagerViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    class ViewPagerViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        private val imageView: ImageView = itemView.findViewById(R.id.ImageView)
+
+        fun bind(url: String) {
+            Glide.with(itemView.context)
+                .load(url)
+                .into(imageView)
+        }
+    }
+}

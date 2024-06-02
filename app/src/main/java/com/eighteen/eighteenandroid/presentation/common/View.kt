533321fc9d93a0a5e644451ac2ce5c4d9fcaaf7a
kim0hoon <gns9460@gmail.com>
2024-06-02 16:38:16 +0900

    revert - View visibility or gone 확장함수 추가(View.isVisible와 중복) https://github.com/LayTheGroundWork/Eighteen_Android/issues/19

--- app/src/main/java/com/eighteen/eighteenandroid/presentation/common/View.kt
+++
@@ -1,7 +0,0 @@
-package com.eighteen.eighteenandroid.presentation.common
-
-import android.view.View
-
-fun View.setVisibleOrGone(isVisible: Boolean) {
-    visibility = if (isVisible) View.VISIBLE else View.GONE
-}

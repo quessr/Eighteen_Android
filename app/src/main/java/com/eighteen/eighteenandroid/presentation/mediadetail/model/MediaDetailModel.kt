ad5eb462ea22ad4f964a11ecd42afc7ac4c95af6
kim0hoon <gns9460@gmail.com>
2024-06-02 17:32:46 +0900

    mediaDetailModel id 필수값으로 변경 https://github.com/LayTheGroundWork/Eighteen_Android/issues/19

--- app/src/main/java/com/eighteen/eighteenandroid/presentation/mediadetail/model/MediaDetailModel.kt
+++ app/src/main/java/com/eighteen/eighteenandroid/presentation/mediadetail/model/MediaDetailModel.kt
@@ -5,14 +5,14 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 sealed class MediaDetailModel : Parcelable {
-    abstract val id: String?
+    abstract val id: String
     abstract val imageUrl: String?
 
-    data class Image(override val id: String? = null, override val imageUrl: String? = null) :
+    data class Image(override val id: String, override val imageUrl: String? = null) :
         MediaDetailModel()
 
     data class Video(
-        override val id: String? = null,
+        override val id: String,
         override val imageUrl: String? = null,
         val videoUrl: String? = null
     ) : MediaDetailModel()

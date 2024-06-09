7cc1bc37cc7cfd82d1291b55932a92b49870f296
kim0hoon <gns9460@gmail.com>
2024-06-09 12:17:01 +0900

    버튼모델 기본 값 추가 https://github.com/LayTheGroundWork/Eighteen_Android/issues/26

--- app/src/main/java/com/eighteen/eighteenandroid/presentation/auth/signup/model/SignUpNextButtonModel.kt
+++ app/src/main/java/com/eighteen/eighteenandroid/presentation/auth/signup/model/SignUpNextButtonModel.kt
@@ -1,12 +1,13 @@
 package com.eighteen.eighteenandroid.presentation.auth.signup.model
 
 import androidx.annotation.StringRes
+import com.eighteen.eighteenandroid.R
 
 data class SignUpNextButtonModel(
-    val isVisible: Boolean,
-    val isEnabled: Boolean,
-    val size: Size,
-    @StringRes val textRes: Int
+    val isVisible: Boolean = false,
+    val isEnabled: Boolean = false,
+    val size: Size = Size.NORMAL,
+    @StringRes val textRes: Int = R.string.sign_up_next
 ) {
     enum class Size {
         FULL,

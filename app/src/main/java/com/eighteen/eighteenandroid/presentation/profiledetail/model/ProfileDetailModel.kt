3e6ff99a8514998bfedb5b7af018a1db94915065
quessr <heosera76@gmail.com>
2024-06-12 08:53:48 +0900

    ProfileDetailModel 추가 #20

---
+++ app/src/main/java/com/eighteen/eighteenandroid/presentation/profiledetail/model/ProfileDetailModel.kt
@@ -0,0 +1,29 @@
+package com.eighteen.eighteenandroid.presentation.profiledetail.model
+
+import com.eighteen.eighteenandroid.presentation.profiledetail.QuestionAnswerAdapter
+
+sealed class ProfileDetailModel {
+    data class ProfileInfo(
+        val name: String,
+        val age: Int,
+        val school: String,
+        val profileImageUrl: ProfileImages) : ProfileDetailModel()
+
+    data class ProfileImages(
+        val imageUrl: List<String>
+    ) : ProfileDetailModel()
+
+    data class Like(
+        val likeCount: Int
+    ) : ProfileDetailModel()
+
+    data class Introduction(
+        val personalityType: String,
+        val introductionText: String
+    ) : ProfileDetailModel()
+
+    data class Qna(
+        val question: String,
+        val answer: String
+    ) : ProfileDetailModel()
+}

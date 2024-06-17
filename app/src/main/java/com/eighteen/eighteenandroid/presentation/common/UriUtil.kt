2cce5a391346e6f833f304c7bd886ced70ff4a83
kim0hoon <gns9460@gmail.com>
2024-06-18 00:33:15 +0900

    getMimeTypeFromUri 출처 추가 https://github.com/LayTheGroundWork/Eighteen_Android/issues/26

--- app/src/main/java/com/eighteen/eighteenandroid/presentation/common/UriUtil.kt
+++ app/src/main/java/com/eighteen/eighteenandroid/presentation/common/UriUtil.kt
@@ -6,7 +6,7 @@ import android.net.Uri
 import android.webkit.MimeTypeMap
 import java.util.Locale
 
-
+//https://stackoverflow.com/questions/8589645/how-to-determine-mime-type-of-file-in-android/31691791#31691791
 fun getMimeTypeFromUri(uri: Uri, context: Context): String? =
     if (ContentResolver.SCHEME_CONTENT == uri.scheme) {
         val cr: ContentResolver = context.contentResolver

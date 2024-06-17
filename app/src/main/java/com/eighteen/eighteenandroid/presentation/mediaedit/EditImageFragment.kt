b16778a7e9daa793f76ad3e0de1f260ff46b0576
kim0hoon <gns9460@gmail.com>
2024-06-17 23:27:13 +0900

    이미지 편집화면 네비게이션 추가 https://github.com/LayTheGroundWork/Eighteen_Android/issues/26

---
+++ app/src/main/java/com/eighteen/eighteenandroid/presentation/mediaedit/EditImageFragment.kt
@@ -0,0 +1,10 @@
+package com.eighteen.eighteenandroid.presentation.mediaedit
+
+import com.eighteen.eighteenandroid.databinding.FragmentEditImageBinding
+import com.eighteen.eighteenandroid.presentation.BaseFragment
+
+class EditImageFragment :
+    BaseFragment<FragmentEditImageBinding>(FragmentEditImageBinding::inflate) {
+    override fun initView() {
+    }
+}

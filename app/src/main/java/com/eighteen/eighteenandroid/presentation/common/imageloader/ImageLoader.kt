1bf0603ca9fe8144e430e234e8e33fe25d654152
kim0hoon <gns9460@gmail.com>
2024-06-22 21:40:53 +0900

    Uri로 이미지 load 추가 https://github.com/LayTheGroundWork/Eighteen_Android/issues/26

--- app/src/main/java/com/eighteen/eighteenandroid/presentation/common/imageloader/ImageLoader.kt
+++ app/src/main/java/com/eighteen/eighteenandroid/presentation/common/imageloader/ImageLoader.kt
@@ -1,10 +1,12 @@
 package com.eighteen.eighteenandroid.presentation.common.imageloader
 
+import android.net.Uri
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 
 interface ImageLoader {
     fun loadUrl(imageView: ImageView, url: String?, placeHolderRes: Int? = null)
+    fun loadUrl(imageView: ImageView, url: Uri?, placeHolderRes: Int? = null)
 
     companion object {
         private val imageLoaderImpl = GlideImageLoader()
@@ -18,6 +20,10 @@ interface ImageLoader {
                     placeholder(it)
                 } ?: this
             }.into(imageView)
+        }
+
+        override fun loadUrl(imageView: ImageView, url: Uri?, placeHolderRes: Int?) {
+            loadUrl(imageView, url.toString(), placeHolderRes)
         }
     }
 }

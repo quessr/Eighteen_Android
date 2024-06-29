78badcfb80c348fbce4128fb8d60528f29018143
kim0hoon <gns9460@gmail.com>
2024-06-29 10:38:29 +0900

    BaseEditMediaFragment property 설명 추가 https://github.com/LayTheGroundWork/Eighteen_Android/issues/26

--- app/src/main/java/com/eighteen/eighteenandroid/presentation/editmedia/BaseEditMediaFragment.kt
+++ app/src/main/java/com/eighteen/eighteenandroid/presentation/editmedia/BaseEditMediaFragment.kt
@@ -5,6 +5,10 @@ import androidx.viewbinding.ViewBinding
 import com.eighteen.eighteenandroid.presentation.BaseFragment
 import com.eighteen.eighteenandroid.presentation.common.viewModelsByBackStackEntryId
 
+/**
+ * @property popDestinationId : ViewModel 공유를 위한 호출자 fragment의 navigation id,
+ * ViewModel 공유를 위해 반드시 넣어야 함
+ */
 abstract class BaseEditMediaFragment<VB : ViewBinding>(bindingFactory: (LayoutInflater) -> VB) :
     BaseFragment<VB>(bindingFactory = bindingFactory) {
     protected val popDestinationId get() = arguments?.getInt(EDIT_MEDIA_POP_DESTINATION_ID_KEY, -1)

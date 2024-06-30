fd951818b2fac3319e4ab2f89a6aa81bde688583
kim0hoon <gns9460@gmail.com>
2024-06-30 12:44:05 +0900

    비디오로부터 썸네일 추출하는 유틸함수 추가 https://github.com/LayTheGroundWork/Eighteen_Android/issues/26

--- app/src/main/java/com/eighteen/eighteenandroid/presentation/common/UriUtil.kt
+++ app/src/main/java/com/eighteen/eighteenandroid/presentation/common/UriUtil.kt
@@ -2,7 +2,10 @@ package com.eighteen.eighteenandroid.presentation.common
 
 import android.content.ContentResolver
 import android.content.Context
+import android.graphics.Bitmap
+import android.media.MediaMetadataRetriever
 import android.net.Uri
+import android.util.Log
 import android.webkit.MimeTypeMap
 import java.util.Locale
 
@@ -19,3 +22,24 @@ fun getMimeTypeFromUri(uri: Uri, context: Context): String? =
             fileExtension.lowercase(Locale.getDefault())
         )
     }
+
+fun getVideoThumbnailFromUri(context: Context, uri: Uri, time: Long): Bitmap? =
+    getVideoThumbnailFromUri(context = context, uri = uri, times = listOf(time)).firstOrNull()
+
+
+fun getVideoThumbnailFromUri(context: Context, uri: Uri, times: List<Long>): List<Bitmap?> {
+    val retriever = MediaMetadataRetriever()
+    val thumbnails = mutableListOf<Bitmap?>()
+    try {
+        retriever.setDataSource(context, uri)
+        times.forEach { time ->
+            val thumbnail = retriever.getFrameAtTime(time)
+            thumbnails.add(thumbnail)
+        }
+    } catch (e: Throwable) {
+        Log.e("getVideoThumbnailFromUri", "${e.message}")
+    } finally {
+        retriever.release()
+    }
+    return thumbnails
+}

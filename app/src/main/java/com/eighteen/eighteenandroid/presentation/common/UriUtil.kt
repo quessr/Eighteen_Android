f434079cb7ea259df4ad35a8f5f1e69b8628c239
kim0hoon <gns9460@gmail.com>
2024-07-04 23:05:22 +0900

    비디오 확장자, path 추출 유틸함수 추가 https://github.com/LayTheGroundWork/Eighteen_Android/issues/26

--- app/src/main/java/com/eighteen/eighteenandroid/presentation/common/UriUtil.kt
+++ app/src/main/java/com/eighteen/eighteenandroid/presentation/common/UriUtil.kt
@@ -5,8 +5,10 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.media.MediaMetadataRetriever
 import android.net.Uri
+import android.provider.MediaStore
 import android.util.Log
 import android.webkit.MimeTypeMap
+import java.io.File
 import java.util.Locale
 
 //https://stackoverflow.com/questions/8589645/how-to-determine-mime-type-of-file-in-android/31691791#31691791
@@ -42,4 +44,24 @@ fun getVideoThumbnailFromUri(context: Context, uri: Uri, times: List<Long>): Lis
         retriever.release()
     }
     return thumbnails
+}
+
+fun contentUriToPath(context: Context, contentUri: Uri): String? {
+    val proj = arrayOf(MediaStore.Images.Media.DATA)
+    val cursor = context.contentResolver.query(contentUri, proj, null, null, null)
+    return cursor?.run {
+        val idx = getColumnIndexOrThrow(MediaStore.MediaColumns.DATA)
+        cursor.moveToFirst()
+        val path = getString(idx)
+        close()
+        path
+    }
+}
+
+fun getFileExtension(context: Context, uri: Uri): String {
+    val extension = if (uri.scheme != null && uri.scheme == ContentResolver.SCHEME_CONTENT) {
+        val mime = MimeTypeMap.getSingleton()
+        mime.getExtensionFromMimeType(context.contentResolver.getType(uri))
+    } else MimeTypeMap.getFileExtensionFromUrl(Uri.fromFile(uri.path?.let { File(it) }).toString())
+    return if (extension.isNullOrEmpty()) ".mp4" else extension
 }

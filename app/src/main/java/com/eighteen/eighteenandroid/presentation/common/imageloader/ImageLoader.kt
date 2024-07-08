7abdd4d400ef3ff2829c29546cb67eabafd48a95
kim0hoon <gns9460@gmail.com>
2024-07-09 00:13:05 +0900

    ImageLoader CenterCrop 추가 https://github.com/LayTheGroundWork/Eighteen_Android/issues/26

--- app/src/main/java/com/eighteen/eighteenandroid/presentation/common/imageloader/ImageLoader.kt
+++ app/src/main/java/com/eighteen/eighteenandroid/presentation/common/imageloader/ImageLoader.kt
@@ -9,6 +9,8 @@ interface ImageLoader {
     fun loadUrl(imageView: ImageView, url: String?, placeHolderRes: Int? = null)
     fun loadUrl(imageView: ImageView, url: Uri?, placeHolderRes: Int? = null)
     fun loadBitmap(imageView: ImageView, bitmap: Bitmap?, placeHolderRes: Int? = null)
+    fun loadUrlCenterCrop(imageView: ImageView, url: String?, placeHolderRes: Int? = null)
+    fun loadUrlCenterCrop(imageView: ImageView, url: Uri?, placeHolderRes: Int? = null)
 
     companion object {
         private val imageLoaderImpl = GlideImageLoader()
@@ -35,5 +37,20 @@ interface ImageLoader {
                 } ?: this
             }.into(imageView)
         }
+
+        override fun loadUrlCenterCrop(imageView: ImageView, url: String?, placeHolderRes: Int?) {
+            Glide.with(imageView).load(url).run {
+                placeHolderRes?.let {
+                    placeholder(it)
+                } ?: this
+            }.centerCrop().into(imageView)
+        }
+
+        override fun loadUrlCenterCrop(imageView: ImageView, url: Uri?, placeHolderRes: Int?) =
+            loadUrlCenterCrop(
+                imageView = imageView,
+                url = url.toString(),
+                placeHolderRes = placeHolderRes
+            )
     }
 }

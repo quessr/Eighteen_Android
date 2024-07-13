7be3fa46e90ef8d5cbece538563344b8371cca94
quessr <heosera76@gmail.com>
2024-07-13 23:37:54 +0900

    Qna 처리에서 QnaList 처리로 변경 #20

--- app/src/main/java/com/eighteen/eighteenandroid/presentation/profiledetail/QuestionAnswerAdapter.kt
+++ app/src/main/java/com/eighteen/eighteenandroid/presentation/profiledetail/QuestionAnswerAdapter.kt
@@ -1,5 +1,6 @@
 package com.eighteen.eighteenandroid.presentation.profiledetail
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.core.view.isVisible
@@ -8,7 +9,7 @@ import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.eighteen.eighteenandroid.databinding.ItemProfileDetailQnaBinding
+import com.eighteen.eighteenandroid.databinding.ItemQnaBinding
 import com.eighteen.eighteenandroid.presentation.profiledetail.model.ProfileDetailModel
 import kotlinx.coroutines.launch
 
@@ -18,15 +19,17 @@ class QuestionAnswerAdapter(
 ) :
     ListAdapter<ProfileDetailModel.Qna, QuestionAnswerAdapter.ViewHolder>(diffUtil) {
 
-    private var items: List<ProfileDetailModel.Qna> = emptyList()
+    private var qnaItems: List<ProfileDetailModel.Qna> = emptyList()
 
 
 
     init {
         lifecycleOwner.lifecycleScope.launch {
             viewModel.items.collect { newItems ->
-                items = newItems.filterIsInstance<ProfileDetailModel.Qna>()
-                notifyDataSetChanged()
+                val qnaLists = newItems.filterIsInstance<ProfileDetailModel.QnaList>()
+                qnaItems = qnaLists.flatMap { it.qnas }
+                Log.d("QuestionAnswerAdapter", "qnaItems size: ${qnaItems.size}")
+                submitList(qnaItems)
             }
         }
 
@@ -39,7 +42,7 @@ class QuestionAnswerAdapter(
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val binding = ItemProfileDetailQnaBinding.inflate(inflater, parent, false)
+        val binding = ItemQnaBinding.inflate(inflater, parent, false)
 
         return ViewHolder(binding).also {
             binding.ivSeeMore.setOnClickListener {
@@ -49,20 +52,20 @@ class QuestionAnswerAdapter(
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        if (position < items.size) {
-            val item = items[position]
-            holder.bind(item.question, item.answer)
-        }
+        val item = getItem(position)
+        holder.bind(item)
 
         val showAllItems = viewModel.showAllItems.value
         val itemCountThreshold = ProfileDetailViewModel.ITEM_COUNT_THRESHOLD
 
+        Log.d("Qna", "Qna items size: ${qnaItems.size}")
+
         // 마지막 아이템 뒤에만 "펼치기"/"접기" 버튼을 보이게 설정
-        if (position == itemCountThreshold - 1 && !showAllItems && items.size > itemCountThreshold) {
+        if (position == itemCountThreshold - 1 && !showAllItems && qnaItems.size > itemCountThreshold) {
             holder.showSeeMore(true)
             holder.setSeeMoreText("펼쳐서 보기")
             holder.seeSeeMoreClickListener { viewModel.toggleItems() }
-        } else if (position == items.size - 1 && showAllItems) {
+        } else if (position == qnaItems.size - 1 && showAllItems) {
             holder.showSeeMore(true)
             holder.setSeeMoreText("접기")
             holder.seeSeeMoreClickListener { viewModel.toggleItems() }
@@ -72,15 +75,23 @@ class QuestionAnswerAdapter(
     }
 
     override fun getItemCount(): Int {
-        return viewModel.getItemCount()
+        val showAllItems = viewModel.showAllItems.value
+        val itemCountThreshold = ProfileDetailViewModel.ITEM_COUNT_THRESHOLD
+        return if (showAllItems) {
+            qnaItems.size
+        } else {
+            minOf(qnaItems.size, itemCountThreshold)
+        }
     }
 
-    class ViewHolder(private val binding: ItemProfileDetailQnaBinding) :
+    class ViewHolder(private val binding: ItemQnaBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun bind(question: String, answer: String) {
-            binding.question.text = question
-            binding.answer.text = answer
+
+        fun bind(qna: ProfileDetailModel.Qna) {
+            binding.question.text = qna.question
+            binding.answer.text = qna.answer
         }
+
 
         fun showSeeMore(show: Boolean) {
             binding.ivSeeMore.isVisible = show

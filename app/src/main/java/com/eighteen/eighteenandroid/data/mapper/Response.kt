5df0070968a8b3d1a7093a54ef0204350540767d
kim0hoon <gns9460@gmail.com>
2024-07-14 16:02:04 +0900

    apiResultMapper 추가(에러처리 필요) https://github.com/LayTheGroundWork/Eighteen_Android/issues/37

--- app/src/main/java/com/eighteen/eighteenandroid/data/mapper/Response.kt
+++ app/src/main/java/com/eighteen/eighteenandroid/data/mapper/Response.kt
@@ -1,11 +1,23 @@
 package com.eighteen.eighteenandroid.data.mapper
 
+import com.eighteen.eighteenandroid.data.datasource.remote.response.ApiResult
 import retrofit2.HttpException
 import retrofit2.Response
 
 fun <I, O> Response<I>.mapper(action: (I) -> O): O {
     return if (this.isSuccessful) {
         this.body()?.let { action(it) } ?: throw Exception("unknown")
+    } else {
+        throw HttpException(this)
+    }
+}
+
+fun <I : ApiResult<T>, T, O> Response<I>.apiResultMapper(action: (I) -> O): O {
+    return if (this.isSuccessful) {
+        //TODO status 값 에러처리
+        val body = body() ?: throw Exception("Unknown")
+        val status = body.status ?: throw Exception("Unknown Status")
+        action(body)
     } else {
         throw HttpException(this)
     }

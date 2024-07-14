e72ce357b45c9962b5641bac7ecad8e3dce73cff
kim0hoon <gns9460@gmail.com>
2024-07-14 18:58:37 +0900

    회원가입 > 약관동의화면 추가 https://github.com/LayTheGroundWork/Eighteen_Android/issues/37

--- app/src/main/java/com/eighteen/eighteenandroid/presentation/auth/signup/termsofservice/SignUpTermsOfServiceFragment.kt
+++ app/src/main/java/com/eighteen/eighteenandroid/presentation/auth/signup/termsofservice/SignUpTermsOfServiceFragment.kt
@@ -1,10 +1,15 @@
 package com.eighteen.eighteenandroid.presentation.auth.signup.termsofservice
 
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import com.eighteen.eighteenandroid.R
 import com.eighteen.eighteenandroid.databinding.FragmentSignUpTermsOfServiceBinding
 import com.eighteen.eighteenandroid.presentation.auth.signup.BaseSignUpContentFragment
 import com.eighteen.eighteenandroid.presentation.auth.signup.model.SignUpNextButtonModel
+import kotlinx.coroutines.launch
 
 /**
  * 약관 동의 페이지
@@ -21,11 +26,41 @@ class SignUpTermsOfServiceFragment : BaseSignUpContentFragment<FragmentSignUpTer
     override val progress: Int? = null
     override val signUpNextButtonModel = SignUpNextButtonModel(
         isVisible = true,
-        isEnabled = true,
+        isEnabled = false,
         size = SignUpNextButtonModel.Size.FULL,
         buttonText = SignUpNextButtonModel.ButtonText.NEXT
     )
 
+    private val signUpTermsOfServiceViewModel by viewModels<SignUpTermsOfServiceViewModel>()
+
     override fun initView() {
+        bind {
+            vBtnCheckAll.setOnClickListener { signUpTermsOfServiceViewModel.checkAll() }
+            vBtnCheckTermsOfService.setOnClickListener { signUpTermsOfServiceViewModel.checkTermsOfService() }
+            vBtnCheckPrivacyPolicy.setOnClickListener { signUpTermsOfServiceViewModel.checkPrivacyPolicy() }
+            vBtnCheckNotification.setOnClickListener { signUpTermsOfServiceViewModel.checkNotification() }
+        }
+        initStateFlow()
+    }
+
+    //TODO 웹뷰 추가
+    private fun initStateFlow() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                signUpTermsOfServiceViewModel.termsOfServiceModelStateFlow.collect {
+                    bind {
+                        vBtnCheckAll.isSelected = it.isCheckedAll
+                        vBtnCheckTermsOfService.isSelected = it.isCheckedTermsOfService
+                        vBtnCheckPrivacyPolicy.isSelected = it.isCheckedPrivacyPolicy
+                        vBtnCheckNotification.isSelected = it.isCheckedNotification
+                    }
+                    signUpViewModelContentInterface.setNextButtonModel(
+                        signUpNextButtonModel = signUpNextButtonModel.copy(
+                            isEnabled = it.isCheckedRequiredAll
+                        )
+                    )
+                }
+            }
+        }
     }
 }

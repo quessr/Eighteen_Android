e4cc311fbca93478b8eaf4dbe9ae0bc4215c405a
kim0hoon <gns9460@gmail.com>
2024-07-14 11:09:44 +0900

    BaseResponse -> ApiResult Wrapper class로 변경 https://github.com/LayTheGroundWork/Eighteen_Android/issues/37

--- app/src/main/java/com/eighteen/eighteenandroid/data/datasource/remote/response/BaseResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.eighteen.eighteenandroid.data.datasource.remote.response
-
-interface BaseResponse<T> {
-    val status: String?
-    val data: T?
-    val message: String?
-}
--- app/src/main/java/com/eighteen/eighteenandroid/data/datasource/remote/service/MessageService.kt
+++ app/src/main/java/com/eighteen/eighteenandroid/data/datasource/remote/service/MessageService.kt
@@ -1,12 +1,12 @@
 package com.eighteen.eighteenandroid.data.datasource.remote.service
 
 import com.eighteen.eighteenandroid.data.datasource.remote.request.SendMessageRequest
-import com.eighteen.eighteenandroid.data.datasource.remote.response.SendMessageResponse
+import com.eighteen.eighteenandroid.data.datasource.remote.response.ApiResult
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface MessageService {
     @POST("/v1/api/message/send")
-    suspend fun postSendMessage(@Body sendMessageRequest: SendMessageRequest): Response<SendMessageResponse>
+    suspend fun postSendMessage(@Body sendMessageRequest: SendMessageRequest): Response<ApiResult<String>>
 }

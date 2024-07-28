5483d2b87faef6fa1cdd13a0752c2684a1006dd9
kim0hoon <gns9460@gmail.com>
2024-07-28 18:44:52 +0900

    내 프로필 > 자기소개 편집 백버튼 callback 추가 #44

--- app/src/main/java/com/eighteen/eighteenandroid/presentation/myprofile/editintroduce/EditIntroduceFragment.kt
+++ app/src/main/java/com/eighteen/eighteenandroid/presentation/myprofile/editintroduce/EditIntroduceFragment.kt
@@ -1,22 +1,41 @@
 package com.eighteen.eighteenandroid.presentation.myprofile.editintroduce
 
+import android.os.Bundle
+import android.view.View
+import androidx.activity.OnBackPressedCallback
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import com.eighteen.eighteenandroid.databinding.FragmentEditIntroduceBinding
 import com.eighteen.eighteenandroid.presentation.BaseFragment
 import com.eighteen.eighteenandroid.presentation.myprofile.editintroduce.model.EditIntroducePage
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
-//TODO mbti item decoration, onBackPressed
+//TODO mbti item decoration
 @AndroidEntryPoint
 class EditIntroduceFragment :
     BaseFragment<FragmentEditIntroduceBinding>(FragmentEditIntroduceBinding::inflate) {
 
     private val editIntroduceViewModel by viewModels<EditIntroduceViewModel>()
+
+    private val onBackPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            when (editIntroduceViewModel.pageStateFlow.value) {
+                EditIntroducePage.MBTI -> findNavController().popBackStack()
+                EditIntroducePage.DESCIRPTION -> editIntroduceViewModel.movePrevPage()
+            }
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        activity?.onBackPressedDispatcher?.addCallback(onBackPressedCallback)
+    }
+
     override fun initView() {
         initMbtiView()
         initDescriptionView()
@@ -53,5 +72,10 @@ class EditIntroduceFragment :
                 }
             }
         }
+    }
+
+    override fun onDestroyView() {
+        onBackPressedCallback.remove()
+        super.onDestroyView()
     }
 }

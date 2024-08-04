7c821d90a532f752247cb90d5dd66b2010e9a34a
kim0hoon <gns9460@gmail.com>
2024-08-04 16:27:31 +0900

    Chat 화면 추가 및 이동 #51

---
+++ app/src/main/java/com/eighteen/eighteenandroid/presentation/chat/ChatFragment.kt
@@ -0,0 +1,10 @@
+package com.eighteen.eighteenandroid.presentation.chat
+
+import com.eighteen.eighteenandroid.databinding.FragmentChatBinding
+import com.eighteen.eighteenandroid.presentation.BaseFragment
+
+class ChatFragment : BaseFragment<FragmentChatBinding>(FragmentChatBinding::inflate) {
+    override fun initView() {
+    }
+
+}

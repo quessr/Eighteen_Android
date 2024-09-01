0dc196894e3c7f06858aec88680b1d69fe7da09e
kim0hoon <gns9460@gmail.com>
2024-09-01 14:19:39 +0900

    MediaPicker single top으로 변경 #26

--- app/src/main/java/com/eighteen/eighteenandroid/presentation/common/mediapicker/MediaPicker.kt
+++ app/src/main/java/com/eighteen/eighteenandroid/presentation/common/mediapicker/MediaPicker.kt
@@ -1,5 +1,8 @@
 package com.eighteen.eighteenandroid.presentation.common.mediapicker
 
+import android.content.Context
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_SINGLE_TOP
 import android.net.Uri
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.PickVisualMediaRequest
@@ -7,24 +10,36 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 
-class MediaPicker {
+open class MediaPicker {
     private val pickMedia: ActivityResultLauncher<PickVisualMediaRequest>
 
     constructor(activity: AppCompatActivity, callback: (Uri?) -> Unit) {
         pickMedia =
-            activity.registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+            activity.registerForActivityResult(resultLauncher) { uri ->
                 callback.invoke(uri)
             }
     }
 
     constructor(fragment: Fragment, callback: (Uri?) -> Unit) {
         pickMedia =
-            fragment.registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+            fragment.registerForActivityResult(resultLauncher) { uri ->
                 callback.invoke(uri)
             }
     }
 
     fun openMediaPicker() {
-        pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageAndVideo))
+        pickMedia.launch(pickVisualMediaRequest)
+    }
+
+    companion object {
+        private val pickVisualMediaRequest =
+            PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageAndVideo)
+        private val resultLauncher = object : ActivityResultContracts.PickVisualMedia() {
+            override fun createIntent(context: Context, input: PickVisualMediaRequest): Intent {
+                return super.createIntent(context, input).apply {
+                    addFlags(FLAG_ACTIVITY_SINGLE_TOP)
+                }
+            }
+        }
     }
 }

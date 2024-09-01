7fba4ddebf95b82064a2e2e5b88b4cf524048614
quessr <heosera76@gmail.com>
2024-09-01 20:11:07 +0900

    RankingFragment 생성 및 NavGraph에 연결, 바텀 네비게이션 가시성 추가 #64

---
+++ app/src/main/java/com/eighteen/eighteenandroid/presentation/ranking/RankingFragment.kt
@@ -0,0 +1,9 @@
+package com.eighteen.eighteenandroid.presentation.ranking
+
+import com.eighteen.eighteenandroid.databinding.FragmentRankingBinding
+import com.eighteen.eighteenandroid.presentation.BaseFragment
+
+class RankingFragment : BaseFragment<FragmentRankingBinding>(FragmentRankingBinding::inflate) {
+    override fun initView() {
+    }
+}

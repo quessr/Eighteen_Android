92076a4876bd4fa93e8310eca15fc2028dad6da0
kim0hoon <gns9460@gmail.com>
2024-09-10 23:07:03 +0900

    SnsLink Deprecated, SnsInfo 추가 #44

--- app/src/main/java/com/eighteen/eighteenandroid/domain/model/Profile.kt
+++ app/src/main/java/com/eighteen/eighteenandroid/domain/model/Profile.kt
@@ -1,5 +1,6 @@
 package com.eighteen.eighteenandroid.domain.model
 
+//TODO snsLinks 제거(Deprecated), snsInfo 기본값 제거
 data class Profile(
     val nickName: String,
     val age: Int,
@@ -8,7 +9,8 @@ data class Profile(
     val teenDescription: String?,
     val medias: List<Media>,
     val id: String,
-    val snsLinks: List<SnsLink>,
+    val snsLinks: List<SnsLink> = emptyList(),
+    val snsInfo: SnsInfo = SnsInfo(null, null, null, null),
     val mbti: Mbti?,
     val description: String?,
     val qna: List<Qna>
---
+++ app/src/main/java/com/eighteen/eighteenandroid/domain/model/SnsInfo.kt
@@ -0,0 +1,8 @@
+package com.eighteen.eighteenandroid.domain.model
+
+data class SnsInfo(
+    val instagram: String?,
+    val x: String?,
+    val tiktok: String?,
+    val youtube: String?
+)

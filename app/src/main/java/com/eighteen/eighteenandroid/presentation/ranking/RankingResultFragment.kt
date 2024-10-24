600aa370d490d1e11b3beca4f595dd777766dddf
yys <yys>
2024-10-24 21:48:03 +0900

    랭킹 결과화면 #76
    Signed-off-by: yys <yys>

---
+++ app/src/main/java/com/eighteen/eighteenandroid/presentation/ranking/RankingResultFragment.kt
@@ -0,0 +1,14 @@
+package com.eighteen.eighteenandroid.presentation.ranking
+
+import com.eighteen.eighteenandroid.databinding.FragmentRankingResultBinding
+import com.eighteen.eighteenandroid.presentation.BaseFragment
+
+class RankingResultFragment : BaseFragment<FragmentRankingResultBinding>(FragmentRankingResultBinding::inflate) {
+
+    override fun initView() {
+
+        bind {
+
+        }
+    }
+}

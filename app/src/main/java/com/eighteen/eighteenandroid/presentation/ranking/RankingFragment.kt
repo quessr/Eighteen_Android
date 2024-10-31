1ed9da6a7f49a6290405e45856e8e8890a509bea
quessr <heosera76@gmail.com>
2024-10-31 22:56:15 +0900

    랭킹화면 투표 카드 클릭 시 VotingDialog 표시 기능 추가 #64

--- app/src/main/java/com/eighteen/eighteenandroid/presentation/ranking/RankingFragment.kt
+++ app/src/main/java/com/eighteen/eighteenandroid/presentation/ranking/RankingFragment.kt
@@ -1,28 +1,47 @@
 package com.eighteen.eighteenandroid.presentation.ranking
 
+import androidx.navigation.fragment.findNavController
+import com.eighteen.eighteenandroid.R
 import com.eighteen.eighteenandroid.common.enums.Tag
 import com.eighteen.eighteenandroid.databinding.FragmentRankingBinding
 import com.eighteen.eighteenandroid.presentation.BaseFragment
 import com.eighteen.eighteenandroid.presentation.common.createChip
 import com.eighteen.eighteenandroid.presentation.common.setTagStyle
+import com.eighteen.eighteenandroid.presentation.common.showDialogFragment
+import com.eighteen.eighteenandroid.presentation.ranking.cardList.CardListAdapter
 import com.eighteen.eighteenandroid.presentation.ranking.cardList.model.CardListItem
 import com.eighteen.eighteenandroid.presentation.ranking.model.RankingCategory
 import com.google.android.material.chip.Chip
 
 class RankingFragment : BaseFragment<FragmentRankingBinding>(FragmentRankingBinding::inflate) {
     private var selectedChip: Chip? = null
+    private lateinit var categories: List<RankingCategory.Category>
     override fun initView() {
-        val adapter = RankingAdapter()
+//        val adapter = RankingAdapter()
+
+        val adapter = RankingAdapter { voteCard ->
+            val category = categories.find { category ->
+                category.cardListItems.any { it.id == voteCard.id }
+            }
+            val categoryTitle = category?.categoryTitle ?: "기본 카테고리"
+
+            val votingDialog = RankingVotingDialogFragment.newInstance(
+                requestKey = REQUEST_KEY_VOTING_ROOM_ENTER,
+                id = voteCard.id,
+                categoryTitle = categoryTitle
+            )
+            showDialogFragment(votingDialog)
+        }
         initChipGroup()
 
 
         binding.rvRanking.run {
             this.adapter = adapter
         }
-        
+
         val categoryTitles = listOf("운동", "뷰티", "공부", "예술", "게임")
 
-        val categories = (0..9).map { index ->
+        categories = (0..9).map { index ->
             val titleIndex = index % categoryTitles.size
             RankingCategory.Category(
                 id = index.toString(),
@@ -61,14 +80,21 @@ class RankingFragment : BaseFragment<FragmentRankingBinding>(FragmentRankingBind
             )
         }
 
+//        val cardListItems = categories.flatMap { it.cardListItems }
+
         // 데이터를 어댑터에 설정
         adapter.submitList(categories)
+        initFragmentResultListener()
     }
 
     private fun initChipGroup() {
         for (tag in Tag.values()) {
+            if (tag == Tag.ALL) {
+                continue
+            }
+
             val chip = createChip(requireContext(), tag.strValue)
-            if (tag == Tag.ALL) { // 화면 최초 진입 시 전체 태그가 클릭된 상태여야함
+            if (tag == Tag.BEAUTY) { // 화면 최초 진입 시 전체 태그가 클릭된 상태여야함
                 chip.setTagStyle(isBlackBackground = true)
                 selectedChip = chip
             }
@@ -81,5 +107,31 @@ class RankingFragment : BaseFragment<FragmentRankingBinding>(FragmentRankingBind
                 chipGroup.addView(chip)
             }
         }
+    }
+
+    private fun initFragmentResultListener() {
+        childFragmentManager.setFragmentResultListener(
+            REQUEST_KEY_VOTING_ROOM_ENTER,
+            viewLifecycleOwner
+        ) { _, result ->
+            val confirmedId = result.getString(RankingVotingDialogFragment.RESULT_CONFIRM_KEY)
+            val canceledId = result.getString(RankingVotingDialogFragment.RESULT_CANCEL_KEY)
+
+            when {
+                confirmedId != null -> {
+                    findNavController().navigate(R.id.fragmentVoting)
+                }
+
+                canceledId != null -> {
+                }
+
+                else -> {
+                }
+            }
+        }
+    }
+
+    companion object {
+        const val REQUEST_KEY_VOTING_ROOM_ENTER = "REQUEST_KEY_VOTING_ROOM_ENTER"
     }
 }

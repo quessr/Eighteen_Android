2b832923e78b66bab707ffdec5f5c82e91e33ce1
yys <yys>
2024-11-03 21:33:58 +0900

    Flow 옵저버 등록 #76
    Signed-off-by: yys <yys>

--- app/src/main/java/com/eighteen/eighteenandroid/presentation/ranking/RankingResultFragment.kt
+++ app/src/main/java/com/eighteen/eighteenandroid/presentation/ranking/RankingResultFragment.kt
@@ -3,6 +3,8 @@ package com.eighteen.eighteenandroid.presentation.ranking
 import androidx.fragment.app.viewModels
 import com.eighteen.eighteenandroid.databinding.FragmentRankingResultBinding
 import com.eighteen.eighteenandroid.presentation.BaseFragment
+import com.eighteen.eighteenandroid.presentation.common.ModelState
+import com.eighteen.eighteenandroid.presentation.common.collectInLifecycle
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -30,6 +32,27 @@ class RankingResultFragment : BaseFragment<FragmentRankingResultBinding>(Fragmen
         bind {
             with(rvRankingResult) {
                 adapter = rankingResultAdapter
+            }
+        }
+
+        initStateFlow()
+    }
+
+    private fun initStateFlow() {
+        collectInLifecycle(viewModel.rankingListStateFlow) {
+            when (it) {
+                is ModelState.Loading -> {
+                    //TODO 로딩 처리
+                }
+                is ModelState.Success -> {
+                    rankingResultAdapter.submitList(it.data)
+                }
+                is ModelState.Error -> {
+                    //TODO 에러처리
+                }
+                is ModelState.Empty -> {
+                    //do nothing
+                }
             }
         }
     }

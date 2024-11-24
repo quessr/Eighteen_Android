d427ef8b36301b76e08987b39eb32f410e5853bd
kim0hoon <gns9460@gmail.com>
2024-11-24 17:55:28 +0900

    테스트 코드 롤백 #84

--- app/src/main/java/com/eighteen/eighteenandroid/data/repository/MessageRepositoryImpl.kt
+++ app/src/main/java/com/eighteen/eighteenandroid/data/repository/MessageRepositoryImpl.kt
@@ -1,35 +1,27 @@
 package com.eighteen.eighteenandroid.data.repository
 
+import com.eighteen.eighteenandroid.data.datasource.remote.request.ConfirmMessageRequest
+import com.eighteen.eighteenandroid.data.datasource.remote.request.SendMessageRequest
 import com.eighteen.eighteenandroid.data.datasource.remote.service.MessageService
+import com.eighteen.eighteenandroid.data.mapper.mapper
 import com.eighteen.eighteenandroid.domain.repository.MessageRepository
 import javax.inject.Inject
 
 class MessageRepositoryImpl @Inject constructor(private val messageService: MessageService) :
     MessageRepository {
-    //    override suspend fun postSendMessage(phoneNumber: String) = runCatching {
-//        messageService.postSendMessage(sendMessageRequest = SendMessageRequest(phoneNumber))
-//            .mapper { it.data }
-//    }
-//
-//    override suspend fun postConfirmMessage(
-//        phoneNumber: String,
-//        verificationCode: String
-//    ) = runCatching {
-//        val request =
-//            ConfirmMessageRequest(phoneNumber = phoneNumber, verificationCode = verificationCode)
-//        messageService.postConfirmMessage(confirmMessageRequest = request).mapper {
-//            it.data
-//        }
-//    }
-    //TODO test 코드 제거
     override suspend fun postSendMessage(phoneNumber: String) = runCatching {
-       ""
+        messageService.postSendMessage(sendMessageRequest = SendMessageRequest(phoneNumber))
+            .mapper { it.data }
     }
 
     override suspend fun postConfirmMessage(
         phoneNumber: String,
         verificationCode: String
     ) = runCatching {
-        ""
+        val request =
+            ConfirmMessageRequest(phoneNumber = phoneNumber, verificationCode = verificationCode)
+        messageService.postConfirmMessage(confirmMessageRequest = request).mapper {
+            it.data
+        }
     }
 }

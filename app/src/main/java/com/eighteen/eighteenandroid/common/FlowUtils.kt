fde4a28490bb32ca5cdae87d835d8c041f5f5165
kim0hoon <gns9460@gmail.com>
2025-01-08 19:42:08 +0900

    suspend 제거

--- app/src/main/java/com/eighteen/eighteenandroid/common/FlowUtils.kt
+++ app/src/main/java/com/eighteen/eighteenandroid/common/FlowUtils.kt
@@ -6,7 +6,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.cancellable
 import kotlinx.coroutines.launch
 
-suspend fun <T> Flow<T>.collectOnce(coroutineScope: CoroutineScope, collect: (T) -> Unit) {
+fun <T> Flow<T>.collectOnce(coroutineScope: CoroutineScope, collect: (T) -> Unit) {
     coroutineScope.launch {
         this@collectOnce.cancellable().collect {
             collect(it)

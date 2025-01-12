0dd4d25c25e1622976ff335d14e61d6de39cacf9
kim0hoon <gns9460@gmail.com>
2025-01-12 16:31:44 +0900

    설정페이지 todo 추가 #44

--- app/src/main/java/com/eighteen/eighteenandroid/presentation/myprofile/setting/SettingFragment.kt
+++ app/src/main/java/com/eighteen/eighteenandroid/presentation/myprofile/setting/SettingFragment.kt
@@ -1,10 +1,23 @@
 package com.eighteen.eighteenandroid.presentation.myprofile.setting
 
+import android.util.Log
+import androidx.navigation.fragment.findNavController
 import com.eighteen.eighteenandroid.databinding.FragmentSettingBinding
 import com.eighteen.eighteenandroid.presentation.BaseFragment
 
-class SettingFragment: BaseFragment<FragmentSettingBinding>(FragmentSettingBinding::inflate) {
+//TODO 버튼 클릭 기능 추가
+class SettingFragment : BaseFragment<FragmentSettingBinding>(FragmentSettingBinding::inflate) {
     override fun initView() {
+        bind {
 
+            ivBtnBack.setOnClickListener {
+                findNavController().popBackStack()
+            }
+            ivBtnManagingInfo.setOnClickListener {
+                //TODO 내 정보 관리
+                Log.d("SettingFragment", "내 정보 관리")
+            }
+
+        }
     }
 }
